c1b4b6c10e6e52fdc89c1e24fac8df3fc2fb7701
Tatiana Kh <ts.khol@gmail.com>
2026-02-14 14:47:44 +0300

    init

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "habits-vue";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      devShell = pkgs.mkShell {
+        buildInputs = [
+          pkgs.nodejs_20
+        ];
+
+        shellHook = ''
+          export VITE_BACK=http://habits.lcl:5000
+        '';
+      };
+
+      packages.default = pkgs.buildNpmPackage {
+        name = "habits-vue";
+
+        buildInputs = with pkgs; [
+          nodejs_20
+        ];
+
+        src = self;
+
+        npmDepsHash = "sha256-Nvb6TkkCiOA68G27mq549hKlEz4mWBuzVia8dpMKmwI=";
+
+        VITE_BACK = "https://api.habits.rusty-cluster.net";
+        npmBuild = "npm run build";
+
+        installPhase = ''
+          mkdir $out
+          cp -r dist/* $out/
+        '';
+      };
+    });
+}
