53f23a42b9afef82445d69300ce9fa516b50460d
Tatiana Kh <ts.khol@gmail.com>
2026-06-26 21:53:49 +0300

    debug

--- src/utils/habitsWeek.js
+++ src/utils/habitsWeek.js
@@ -1,12 +1,21 @@
-const isMetricSetForDateAndHabit = (metric, habitId, weekDate) =>
-  metric.habit_id === habitId &&
-  new Date(metric.date).toDateString() ===
-  weekDate.toDateString();
+const isMetricSetForDateAndHabit = (metric, habitId, weekDate) =>{ 
+  console.log(weekDate.toDateString())
+  console.log(new Date(metric.date).toDateString())
+  console.log(new Date(metric.date).toDateString() === weekDate.toDateString())
+
+  console.log(habitId)
+  console.log(metric.habit_id)
+
+  // FIXME
+  return metric.habit_id == habitId &&
+  new Date(metric.date).toDateString() === weekDate.toDateString()
+}
 
 export const makeHabitsWeek = (habits, metrics, days) => {
   return habits.map(habit => ({
     ...habit,
     metrics: days.map(day => {
+      // console.log(day)
       const found = metrics.find(m =>
         isMetricSetForDateAndHabit(m, habit.id, day)
       )
